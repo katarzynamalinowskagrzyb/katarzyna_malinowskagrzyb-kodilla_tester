24c328731a87a178925af2cc66a0d5209eb6d1ef
Katarzyna Malinowska-Grzyb <katarzyna.malinow@gmail.com>
2022-10-22 23:45:36 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String [] args) {
+      String clientAddress = "Plac Bankowy 13";
+      int clientType = 24;
+      double clientInterestRate = 1.24;
+      char clientFeature = 'K';
+      boolean isNewClient = false;
+      String clientCity = "Warsaw";
+      System.out.println(clientAddress);
+      System.out.println(clientType);
+      System.out.println(clientInterestRate);
+      System.out.println(clientFeature);
+      System.out.println(isNewClient);
+      System.out.println(clientCity);
+    }
+}
