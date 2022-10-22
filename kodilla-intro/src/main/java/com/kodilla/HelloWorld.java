263381d2e983e2d54f6b9d8bc1b26406805163c1
Katarzyna Malinowska-Grzyb <katarzyna.malinow@gmail.com>
2022-10-22 22:42:26 +0200

    Java HelloWorld, First Class, Gradle

---
+++ kodilla-intro/src/main/java/com/kodilla/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+public class HelloWorld {
+    //klasa HelloWorld
+    public static void main(String[] args) {
+        //definiacja metody main
+        System.out.println("Hello World!");
+        System.out.println("Katarzyna");
+        System.out.println(36);
+        System.out.println('G');
+        //wywołanie metody main
+    }
+}
