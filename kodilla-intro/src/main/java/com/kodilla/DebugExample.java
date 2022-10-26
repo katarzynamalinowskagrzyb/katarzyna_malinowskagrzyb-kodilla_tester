b6522edf1a88e423b82c5a38a532febb82031fa4
Katarzyna Malinowska-Grzyb <katarzyna.malinow@gmail.com>
2022-10-26 22:58:27 +0200

    DebugExample if/else

--- kodilla-intro/src/main/java/com/kodilla/DebugExample.java
+++ kodilla-intro/src/main/java/com/kodilla/DebugExample.java
@@ -5,16 +5,19 @@ public class DebugExample {
 
         System.out.println(firstNumber);
         System.out.println(secondNumber);
-
-        sumAndDisplay(firstNumber, secondNumber);
+        if (firstNumber > secondNumber) {
+            subtractAndDisplay(firstNumber,secondNumber);
+        } else {
+            sumAndDisplay(firstNumber, secondNumber);
+        }
     }
-
     private static void sumAndDisplay(int a, int b) {
         int result = a + b;
 
         System.out.println(result);
-
-
     }
-
+    private static void subtractAndDisplay(int a, int b) {
+        int result = a - b;
+        System.out.println(result);
+    }
 }
