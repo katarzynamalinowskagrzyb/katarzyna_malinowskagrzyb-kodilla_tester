0ce1097262003449e5b511802882610116319c26
Katarzyna Malinowska-Grzyb <katarzyna.malinow@gmail.com>
2022-10-30 15:04:08 +0100

    SimpleArray, LeapYear

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,17 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] ourAnimals = new String[5];
+        ourAnimals[0] = "Cat";
+        ourAnimals[1] = "Dog";
+        ourAnimals[2] = "Bird";
+        ourAnimals[3] = "Flamingo";
+        ourAnimals[4] = "Lion";
+       // full commend: String[] ourAnimals = new ourAnimals[] {"Cat", "Dog", "Bird", "Flamingo", "Lion"};
+
+
+        String Animal = ourAnimals[3];
+        System.out.println( "Animal in the index of 3 is" + Animal);
+        int numberOfElements = ourAnimals.length;
+        System.out.println("Moja tablica zawiera " + numberOfElements + " znaków" );
+    }
+}
--- kodilla-intro/src/main/java/com/kodilla/LeapYear.java
+++ kodilla-intro/src/main/java/com/kodilla/LeapYear.java
@@ -1,17 +1,25 @@
 package com.kodilla;
 
 public class LeapYear {
-    public static void main(String[] args){
-        int exampleYear = 2022;
+    public static boolean check(int exampleYear){
+        //publiczna statyczna metoda zwarająca boolean o nazwie main przyjmująca argument rok
 
         if(exampleYear % 4 !=0) {
+            return false;
         } else if (exampleYear % 100 != 0) {
-            System.out.println("Leap Year");
+            return true;
         } else if (exampleYear % 400 == 0) {
-            System.out.println("Leap Year");
+            return true;
+        }   else {
+            return false;
         }
     }
+    public static void main(String[] args) {
+        System.out.println(check(2000));
+        //check(2000);
+    }
 }
+
 
 
 
